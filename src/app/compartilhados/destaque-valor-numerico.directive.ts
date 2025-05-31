3d1093ef92edebf3b49d351f331d37b7f9acace3
Carlos6464 <adrianosodre23@gmail.com>
2025-05-31 15:55:19 -0300

    Add destaque-valor-numerico directive for dynamic styling of financial values
    - Updated saldo component to use the new directive for highlighting positive and negative values.
    - Modified transacao component to apply the directive for value styling.
    - Initialized saldo with a default value of 1000.
    - Enhanced HTML templates to integrate the directive for improved visual feedback on financial data.

---
+++ src/app/compartilhados/destaque-valor-numerico.directive.ts
@@ -0,0 +1,26 @@
+import { afterRender, Directive, ElementRef, input } from "@angular/core";
+
+@Directive({
+    selector: '[appDestaqueValorNumerico]'
+})
+export class DestaqueValorNumericoDirective {
+
+    appDestaqueValorNumerico = input.required<number>();
+
+    corPositiva = input("var(--destaque-receita)");
+    corNegativa = input("var(--destaque-despesa)");
+    constructor(elemento: ElementRef<HTMLElement>) { 
+        afterRender(() => {
+
+            if (this.appDestaqueValorNumerico() < 0) {
+                elemento.nativeElement.style.color = this.corNegativa();
+                return;
+            }else if (this.appDestaqueValorNumerico() > 0) {
+                elemento.nativeElement.style.color = this.corPositiva();
+                return;
+            }
+            
+        })
+        
+    }
+}
